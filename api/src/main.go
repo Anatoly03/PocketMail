114f192d3ceee5239370edfbfcc7ebc6bd7d065c
Anatoly Wein <anatoly.wein@gmail.com>
2026-02-27 16:28:42 +0100

    add snapshot of pocketbase database

--- api/src/main.go
+++ api/src/main.go
@@ -9,16 +9,27 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 var IsDev = strings.HasPrefix(os.Args[0], os.TempDir())
 
 func main() {
 	_ = godotenv.Load(".env")
+
+    // loosely check if it was executed using "go run"
+    isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
 	
 	// Create a new pocketbase instance.
 	app := pocketbase.New()
 	s := mail.MailServer{ App: app }
+
+	// register the migrate command plugin
+    migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+        // enable auto creation of migration files when making collection changes in the Dashboard
+        // (the isGoRun check is to enable it only during development)
+        Automigrate: isGoRun,
+    })
 
 	// Start the SMTP server in a separate goroutine.
 	go s.Serve()
