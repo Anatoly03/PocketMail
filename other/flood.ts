b5a71374952fc18ba4bfb33eee3c423a15f6042d
Anatoly Wein <anatoly.wein@gmail.com>
2026-02-27 20:20:15 +0100

    refactor flood

--- other/flood.ts
+++ other/flood.ts
@@ -1,14 +1,14 @@
-import "dotenv/config"
-import readline from "node:readline"
-import { faker } from "@faker-js/faker"
-import * as nodemailer from "nodemailer"
+import "dotenv/config";
+import readline from "node:readline";
+import { faker } from "@faker-js/faker";
+import * as nodemailer from "nodemailer";
 
-const noAsk = process.argv.includes("--no-ask")
+const noAsk = process.argv.includes("--no-ask");
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-})
+});
 
 const transporter = nodemailer.createTransport({
     host: "localhost",
@@ -16,47 +16,48 @@ const transporter = nodemailer.createTransport({
     secure: false,
     auth: undefined,
     newline: "unix",
-})
+});
 
 function questionPromise(query: string, defaultAnswer: string): Promise<string> {
     return new Promise((resolve) => {
         if (noAsk) {
-            resolve(defaultAnswer)
+            resolve(defaultAnswer);
         } else {
             rl.question(query + ": ", (answer) => {
-                resolve(answer || defaultAnswer)
-            })
+                resolve(answer || defaultAnswer);
+            });
         }
-    })
+    });
 }
 
 async function generateFake() {
-    let to = await questionPromise("Email", "test@localhost")
-    let toName = to.split("@")[0]
-    const amountString = await questionPromise("Amount", "50")
-    const amount = parseInt(amountString, 10)
+    let to = await questionPromise("Email", "test@localhost");
+    let toName = to.split("@")[0];
+    const amountString = await questionPromise("Amount", "50");
+    const amount = parseInt(amountString, 10);
 
     if (!to.includes("@")) {
-        toName = to
-        to += "@localhost"
+        toName = to;
+        to += "@localhost";
     }
 
-    console.debug(`sending ${amount} emails to: \`${to}\``)
+    console.debug(`sending ${amount} emails to: \`${to}\``);
 
     for (let i = 0; i < amount; i++) {
-        const firstName = faker.person.firstName()
-        const lastName = faker.person.lastName()
-        const fakeEmail = faker.internet.email({ firstName, lastName })
+        const firstName = faker.person.firstName();
+        const lastName = faker.person.lastName();
+        const fakeEmail = faker.internet.email({ firstName, lastName });
 
-        const subject = faker.lorem.sentence()
-        const sent = faker.date.past().toISOString()
-        
-        const randomBody = faker.lorem.paragraphs({ min: 1, max: 20 })
-            .split('\n')
-            .map((p) => `<p>${faker.lorem.words({ min: p.split(' ').length, max: p.split(' ').length * 6 })}</p>`)
-            .join('')
-        const toNameCapitalized = toName.charAt(0).toUpperCase() + toName.slice(1)
-        const text = `<p>Dear ${toNameCapitalized},</p>${randomBody} <p>Best Regards,<br>${firstName} ${lastName}</p>`
+        const subject = faker.lorem.sentence();
+        const sent = faker.date.past().toISOString();
+
+        const randomBody = faker.lorem
+            .paragraphs({ min: 1, max: 20 })
+            .split("\n")
+            .map((p) => `<p>${faker.lorem.words({ min: p.split(" ").length, max: p.split(" ").length * 6 })}</p>`)
+            .join("");
+        const toNameCapitalized = toName.charAt(0).toUpperCase() + toName.slice(1);
+        const text = `<p>Dear ${toNameCapitalized},</p>${randomBody} <p>Best Regards,<br>${firstName} ${lastName}</p>`;
 
         const info = await transporter.sendMail({
             from: `${firstName} ${lastName} <${fakeEmail}>`, // Visible From: header
@@ -68,14 +69,12 @@ async function generateFake() {
             },
             subject,
             text,
-        })
+        });
 
-        if (!noAsk) {
-            console.log(info.messageId)
-        }
+        console.log(info.response, info.messageId);
     }
 
-    rl.close()
+    rl.close();
 }
 
-generateFake()
+generateFake();
